c0cd0b9128ee89ce0d9018811b3561c13f274111
Onomesino <onomesinoesther@gmail.com>
2022-09-13 12:42:23 +0000

    amazzon

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hey there, I'm cloning amazon 🥳</h1>
+      {/* Header */}
+      {/* Home */}
+      
+    </div>
+  );
+}
+
+export default App;
